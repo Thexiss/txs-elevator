1facd96707a3b0a6bf632e18e892c155736f5b87
Thexis FiveM <thexis.fivem@gmail.com>
2024-09-22 11:33:03 +0300

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,11 +1,12 @@
 Config = {}
 
 Config.Elevators = {
-    -- ["hospital_elevator"] = {
-    --     label = "Hospital",
-    --     floors = {
-    --         [1] = {label = "Lobby", coords = {x = 338.84, y = -1394.56, z = 32.5}},
-    --         [2] = {label = "First Floor", coords = {x = 338.85, y = -1394.56, z = 39.00}},
-    --     }
-    -- }
+    ["hospital_elevator"] = {
+        label = "Hospital",
+        targetLabel = "Elevator",
+        floors = {
+            [1] = {coords = vector4(359.92, -585.01, 28.82, 253.05), targetCoords = vector4(359.91, -583.99, 28.82, 13.99)},
+            [2] = {coords = vector4(339.82, -584.55, 74.17, 241.59), targetCoords = vector4(338.53, -584.04, 74.17, 63.23)},
+        }
+    }
 }
